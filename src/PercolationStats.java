fa36e22d9b9f38c0c8a32c2d251e1c3fc0f11280
Tyler Crosse <tylerscottcrosse@gmail.com>
2019-01-12 10:13:33 -0800

    Fix issues identified in checkstyles

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -4,9 +4,9 @@
  *  Execution:    java PercolationStats
  *  Dependencies: Percolation, algs4.StdOut, algs4.StdRandom, algs4.StdStats
  *
- *  This program method that takes two command-line arguments n and T, 
- *  performs T independent computational experiments (discussed above) on an 
- *  n-by-n grid, and prints the sample mean, sample standard deviation, 
+ *  This program method that takes two command-line arguments n and T,
+ *  performs T independent computational experiments (discussed above) on an
+ *  n-by-n grid, and prints the sample mean, sample standard deviation,
  *  and the 95% confidence interval for the percolation threshold.
  *
  ******************************************************************************/
@@ -15,32 +15,49 @@ import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
+/**
+ * This class takes two command-line arguments n and T, performs T
+ * independent computational experiments (discussed above) on an n-by-n grid,
+ * and prints the sample mean, sample standard deviation, and the 95% confidence
+ * interval for the percolation threshold.
+ *
+ * @author Tyler Crosse
+ */
 public class PercolationStats {
+  /**
+   * Dimensions of percolation grid to test.
+   */
   private int size;
+  /**
+   * Number of trials.
+   */
   private int t;
+  /**
+   * Threshold for each experiment.
+   */
   private double[] thresholds;
 
   /**
-   * perform trials independent experiments on an n-by-n grid
-   * 
-   * @param n
-   * @param trials
+   * Perform trials independent experiments on an n-by-n grid.
+   *
+   * @param n dimnesions of n by n grid
+   * @param trials number of trials to run
    */
-  public PercolationStats(int n, int trials) {
+  public PercolationStats(final int n, final int trials) {
     validate(n, trials);
     size = n;
     t = trials;
     thresholds = new double[t];
-    
+
     for (int i = 0; i < trials; i++) {
       thresholds[i] = trial();
     }
   }
 
   /**
-   * performs a single trial
-   * 
-   * @param i
+   * Performs a single trial. Returns the threshold.
+   *
+   * @return threshold for trial
    */
   private double trial() {
     Percolation perc = new Percolation(size);
@@ -57,19 +74,18 @@ public class PercolationStats {
     }
     return runs / (Math.pow(size, 2));
   }
-  
+
   /**
-   * sample mean of percolation threshold
-   * 
-   * @return
+   * Sample mean of percolation threshold.
+   * @return mean of percolation thresholds
    */
   public double mean() {
     return StdStats.mean(thresholds);
   }
-  
+
   /**
-   * sample standard deviation of percolation threshold
-   * @return
+   * Sample standard deviation of percolation threshold.
+   * @return standard deviation of thresholds from all the completed trials
    */
   public double stddev() {
     if (t == 1) {
@@ -79,45 +95,58 @@ public class PercolationStats {
   }
 
   /**
-   * low  endpoint of 95% confidence interval
-   * 
-   * @return
+   * Low endpoint of 95% confidence interval.
+   * @return Low endpoint of 95% confidence interval
    */
   public double confidenceLo() {
-    return mean() - (1.96 * stddev() / Math.sqrt(t));
+    double confidence95Z = 1.96;
+    return mean() - (confidence95Z * stddev() / Math.sqrt(t));
   }
-  
+
   /**
-   * high endpoint of 95% confidence interval
-   * 
-   * @return
+   * High endpoint of 95% confidence interval.
+   *
+   * @return High endpoint of 95% confidence interval
    */
   public double confidenceHi() {
-    return mean() + (1.96 * stddev() / Math.sqrt(t));
+    double confidence95Z = 1.96;
+    return mean() + (confidence95Z * stddev() / Math.sqrt(t));
   }
 
-  private void validate(int n, int trials) {
+  /**
+   * Validates that the dimensions and  number of trials are greater than zero.
+   * @param n dimensions
+   * @param trials number of trials
+   */
+  private void validate(final int n, final int trials) {
     // java.lang.IllegalArgumentException if either n ≤ 0 or trials ≤ 0.
     if (n <= 0) {
-      throw new IllegalArgumentException("the size needs to be greater than 0");
+      throw new IllegalArgumentException(
+        "the size needs to be greater than 0"
+      );
     }
     if (trials <= 0) {
-      throw new IllegalArgumentException("the number of trials needs to be greater than 0");
+      throw new IllegalArgumentException(
+        "the number of trials needs to be greater than 0"
+      );
     }
   }
 
   /**
-   * test client (described below)
-   * 
-   * @param args
-   * @return
+   * Test client (described below).
+   *
+   * @param args command line arguments
    */
-  public static void main(String[] args) {
+  public static void main(final String[] args) {
     int n = Integer.parseInt(args[0]);
     int trials = Integer.parseInt(args[1]);
     PercolationStats percStats = new PercolationStats(n, trials);
-    StdOut.println("mean                    = "+ percStats.mean());
-    StdOut.println("stddev                  = "+ percStats.stddev());
-    StdOut.println("95% confidence interval = ["+ percStats.confidenceLo()+", "+percStats.confidenceHi()+"]");
+    StdOut.println("mean                    = " + percStats.mean());
+    StdOut.println("stddev                  = " + percStats.stddev());
+    StdOut.println(
+      "95% confidence interval = ["
+      + percStats.confidenceLo() + ", "
+      + percStats.confidenceHi() + "]"
+    );
   }
 }
